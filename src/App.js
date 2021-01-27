f9772d20f621be4b897abf79a5ba071049557482
Hossam Marey <hossam.marey@rytalo.com>
2021-01-27 12:44:41 +0200

    first m

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>src/App.js</code>
         </p>
         <a
           className="App-link"
