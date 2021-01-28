f62b767e79dca21d5692689f70ea6f4c11e66631
Hossam Marey <hossam.marey@rytalo.com>
2021-01-28 16:25:19 +0200

    list rendering

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,8 @@
-import { useState } from "react";
 import Posts from "./posts/Posts";
 
 const Home = () => {
-  // let name = "Ali";
-  let [name, setName] = useState("Ali");
-  const handleClick = () => {
-    // name = "Hossam";
-    setName("Hossam");
-  };
-  console.log(name);
   return (
     <div className="home">
-      {/* <button className="btn" onClick={(e) => handleClick("Hossam", e)}>
-        Click Me
-      </button> */}
-      <button className="btn" onClick={handleClick}>
-        Click Me
-      </button>
-      <h1> {name}</h1>
       <Posts />
     </div>
   );
--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -1,14 +1,27 @@
+import { useState } from "react";
+
 import Post from "./Post";
 
 const Posts = () => {
+  let img1 = "https://images.unsplash.com/photo-1498050108023-c5249f4df085";
+  let img2 = "https://images.unsplash.com/photo-1505740420928-5e560c06d30e";
+  let img3 = "https://images.unsplash.com/photo-1454165205744-3b78555e5572";
+  const [posts, setPost] = useState([
+    { id: 1, title: "post title 1", body: "post body ...", image: img1 },
+    { id: 2, title: "post title 2", body: "post body ...", image: img2 },
+    { id: 3, title: "post title 3", body: "post body ...", image: img3 },
+  ]);
   return (
     <section className="posts">
+      {posts.map((post) => (
+        <Post key={post.id} />
+      ))}
+      {/* <Post />
       <Post />
       <Post />
       <Post />
       <Post />
-      <Post />
-      <Post />
+      <Post /> */}
     </section>
   );
 };
