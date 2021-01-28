f08a15e1b8a49fd6be5aa0d606081361b037c19a
Hossam Marey <hossam.marey@rytalo.com>
2021-01-28 18:35:56 +0200

    props

--- src/components/posts/Post.js
+++ src/components/posts/Post.js
@@ -1,12 +1,12 @@
-const Post = () => {
+const Post = ({ post, handleDelete }) => {
   return (
     <div className="post">
-      <img
-        src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80"
-        alt=""
-      />
-      <div className="post-author">By: Hossam</div>
-      <h3>Lorem ipsum dolor sit amet consectetur adipisicing.</h3>
+      <img src={post.image} alt="" />
+      <div className="post-author">By: {post.author ? post.author : "Ali"}</div>
+      <h3>{post.title}</h3>
+      <button className="btn" onClick={() => handleDelete(post.id)}>
+        Delete
+      </button>
     </div>
   );
 };
--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -7,21 +7,33 @@ const Posts = () => {
   let img2 = "https://images.unsplash.com/photo-1505740420928-5e560c06d30e";
   let img3 = "https://images.unsplash.com/photo-1454165205744-3b78555e5572";
   const [posts, setPost] = useState([
-    { id: 1, title: "post title 1", body: "post body ...", image: img1 },
-    { id: 2, title: "post title 2", body: "post body ...", image: img2 },
+    {
+      id: 1,
+      title: "post title 1",
+      body: "post body ...",
+      image: img1,
+      author: "Admin",
+    },
+    {
+      id: 2,
+      title: "post title 2",
+      body: "post body ...",
+      image: img2,
+      author: "Codv",
+    },
     { id: 3, title: "post title 3", body: "post body ...", image: img3 },
   ]);
+
+  const handleDelete = (id) => {
+    let curPosts = [...posts];
+    let newposts = curPosts.filter((post) => post.id !== id);
+    setPost(newposts);
+  };
   return (
     <section className="posts">
       {posts.map((post) => (
-        <Post key={post.id} />
+        <Post key={post.id} post={post} handleDelete={handleDelete} />
       ))}
-      {/* <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post /> */}
     </section>
   );
 };
