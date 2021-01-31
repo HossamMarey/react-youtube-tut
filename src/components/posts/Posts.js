6af3b478d528895a293572c1c25d80f5cfd957c0
Hossam Marey <hossam.marey@rytalo.com>
2021-01-31 16:38:06 +0200

    conditional messages

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -4,14 +4,26 @@ import Post from "./Post";
 
 const Posts = () => {
   const [posts, setPost] = useState(null);
+  const [isloading, setIsloading] = useState(false);
+  const [errMsg, setErrMsg] = useState(null);
 
   useEffect(() => {
+    setIsloading(true);
     fetch("http://localhost:4000/posts")
       .then((res) => {
+        if (!res.ok) {
+          throw Error("Not Found");
+        }
         return res.json();
       })
       .then((data) => {
         console.log(data);
+        setPost(data);
+        setIsloading(false);
+      })
+      .catch((err) => {
+        setIsloading(false);
+        setErrMsg(err.message);
       });
   }, []);
 
@@ -22,11 +34,15 @@ const Posts = () => {
   };
   return (
     <section className="posts">
-      {posts
-        ? posts.map((post) => (
-            <Post key={post.id} post={post} handleDelete={handleDelete} />
-          ))
-        : "no posts"}
+      {posts &&
+        posts.map((post) => (
+          <Post key={post.id} post={post} handleDelete={handleDelete} />
+        ))}
+      {isloading && <div> loading ... </div>}
+      {!posts && !isloading && !errMsg && (
+        <div className="not-found"> No Posts</div>
+      )}
+      {errMsg && <div className="error"> {errMsg}</div>}
     </section>
   );
 };
