5d78500b3e6cb4aa85cc34f7ad91e1631d3f906c
Hossam Marey <hossam.marey@rytalo.com>
2021-01-31 14:54:25 +0200

    fetch api

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -3,30 +3,17 @@ import { useState, useEffect } from "react";
 import Post from "./Post";
 
 const Posts = () => {
-  let img1 = "https://images.unsplash.com/photo-1498050108023-c5249f4df085";
-  let img2 = "https://images.unsplash.com/photo-1505740420928-5e560c06d30e";
-  let img3 = "https://images.unsplash.com/photo-1454165205744-3b78555e5572";
-  const [posts, setPost] = useState([
-    {
-      id: 1,
-      title: "post title 1",
-      body: "post body ...",
-      image: img1,
-      author: "Admin",
-    },
-    {
-      id: 2,
-      title: "post title 2",
-      body: "post body ...",
-      image: img2,
-      author: "Codv",
-    },
-    { id: 3, title: "post title 3", body: "post body ...", image: img3 },
-  ]);
+  const [posts, setPost] = useState(null);
 
   useEffect(() => {
-    console.log("Empty useEffect");
-  });
+    fetch("http://localhost:4000/posts")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
 
   const handleDelete = (id) => {
     let curPosts = [...posts];
@@ -35,9 +22,11 @@ const Posts = () => {
   };
   return (
     <section className="posts">
-      {posts.map((post) => (
-        <Post key={post.id} post={post} handleDelete={handleDelete} />
-      ))}
+      {posts
+        ? posts.map((post) => (
+            <Post key={post.id} post={post} handleDelete={handleDelete} />
+          ))
+        : "no posts"}
     </section>
   );
 };
